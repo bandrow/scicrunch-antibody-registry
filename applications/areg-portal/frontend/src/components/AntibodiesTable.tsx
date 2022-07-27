2efb2def928e62ffd468b891fbfc11687c38d1c0
Anto <antonella@tryfondo.com>
2022-07-27 13:08:31 +0200

    1/add joint values for name & Id and target antigen & species columns

--- applications/areg-portal/frontend/src/components/AntibodiesTable.tsx
+++ applications/areg-portal/frontend/src/components/AntibodiesTable.tsx
@@ -1,28 +1,39 @@
 import React from "react";
 //MUI
-import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import {
+  DataGrid,
+  GridToolbar,
   GridRowsProp,
   GridColDef,
   GridValueGetterParams,
+  GridRenderCellParams,
 } from "@mui/x-data-grid";
-import Box from "@mui/material/Box";
+import { Typography, Box } from "@mui/material";
 
 //project imports
 import { getAntibodies } from "../utils/helpers";
 import Content from "./UI/Content";
 
-//use renderCell to render diferent types here:
-// renderCell: (params) => (
-//   <div>
-//     <Typography>{params.value.name}</Typography>
-//     <Typography color="textSecondary">{params.value.title}</Typography>
-//   </div>
+const renderNameAndId = (props: GridRenderCellParams<String>) => {
+  return (
+    <Box>
+      <Typography variant="body2">{props.row.ab_name}</Typography>
+      <Typography variant="caption" align="left" component="div">
+        {props.row.ab_id}
+      </Typography>
+    </Box>
+  );
+};
 
-function joinNameAndId(params: GridValueGetterParams) {
-  let nameIdJoint = `${params.row.ab_name || ""} ${params.row.ab_id || ""}`;
-  return nameIdJoint;
-}
+const renderTargetAntigenAndSpecies = (props: GridRenderCellParams<String>) => {
+  return (
+    <Box>
+      <Typography variant="body2">
+        {props.row.ab_target} {props.row.target_species}
+      </Typography>
+    </Box>
+  );
+};
 
 const rows: GridRowsProp = getAntibodies();
 
@@ -30,73 +41,83 @@ const columns: GridColDef[] = [
   {
     field: "ab_name",
     headerName: "Name",
-    minWidth: 100,
+    minWidth: 75,
     flex: 1,
+    hide: true,
   },
   {
     field: "ab_id",
     headerName: "ID",
-    minWidth: 100,
+    minWidth: 75,
     flex: 1,
+    hide: true,
   },
   {
     field: "ab_name_id",
     headerName: "Name & ID",
-    width: 250,
-    valueGetter: joinNameAndId,
+    width: 200,
+    renderCell: renderNameAndId,
   },
   {
     field: "ab_target",
     headerName: "Target antigen",
-    minWidth: 100,
+    minWidth: 75,
     flex: 1,
+    hide: true,
   },
   {
     field: "target_species",
     headerName: "Target species",
-    minWidth: 100,
+    minWidth: 75,
     flex: 1,
+    hide: true,
+  },
+  {
+    field: "target_ant_spec",
+    headerName: "Target antigen & species",
+    width: 200,
+    renderCell: renderTargetAntigenAndSpecies,
   },
   {
     field: "proper_citation",
     headerName: "Proper citation",
-    minWidth: 100,
+    minWidth: 75,
     flex: 1,
   },
   {
     field: "clonality",
     headerName: "Clonality",
-    minWidth: 100,
+    minWidth: 75,
     flex: 1,
   },
   {
     field: "comments",
     headerName: "Comments",
-    minWidth: 100,
+    minWidth: 75,
     flex: 1,
   },
   {
     field: "clone_id",
     headerName: "Clone ID",
-    minWidth: 100,
+    minWidth: 75,
     flex: 1,
   },
   {
     field: "host",
     headerName: "Host",
-    minWidth: 100,
+    minWidth: 75,
     flex: 1,
   },
   {
     field: "vendor",
     headerName: "Vendor",
-    minWidth: 100,
+    minWidth: 75,
     flex: 1,
   },
   {
     field: "catalog_num",
     headerName: "Cat Num",
-    minWidth: 100,
+    minWidth: 75,
     flex: 1,
   },
 ];
